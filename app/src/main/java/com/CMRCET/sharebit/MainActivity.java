43a5ae34dba800d9f3463d072c95f92a47a92175
pranathi99 <pranathi.ganti@gmail.com>
2020-06-01 10:18:33 +0530

    First commit

--- app/src/main/java/com/CMRCET/sharebit/MainActivity.java
+++ app/src/main/java/com/CMRCET/sharebit/MainActivity.java
@@ -2,30 +2,83 @@ package com.CMRCET.sharebit;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText username,password;
     private Button loginButton;
+    private FirebaseAuth mAuth;
+    private DatabaseReference ref= FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.pass);
         loginButton = (Button)findViewById(R.id.loginin);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
             {
-                Intent intent = new Intent(getApplicationContext(),HomePage.class);
-                startActivity(intent);
+                /*ref.child("Users").orderByChild("username").equalTo("501").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        Intent intent = new Intent(getApplicationContext(),HomePage.class);
+                        startActivity(intent);
+                        Toast.makeText(MainActivity.this,"LoggedIn Successfully",Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Account doesn't exist.Contact Administrator", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });*/
+                Query query = ref.child("Users").orderByChild("username").equalTo(username.getText().toString());
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.getChildrenCount() != 0) {
+                            Intent intent = new Intent(getApplicationContext(),HomePage.class);
+                            startActivity(intent);
+                            Toast.makeText(MainActivity.this,"LoggedIn Successfully",Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Account doesn't exist.Contact Administrator", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
-
     }
 
     @Override
